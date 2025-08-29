5a92e418865394668732d4faeaa4c20921387bcd
jensadev <jens.andreasson@ntig.se>
2025-08-29 08:49:52 +0200

    best of 3

--- prog.py
+++ prog.py
@@ -3,16 +3,23 @@ print("Välkommen till rulla tärningsspelet!")
 spelare1 = input("Spelare 1, var god skriv ditt namn: ")
 print(f"Hej {spelare1}, nu är det dax att rulla tärning.")
 spela = True
-while spela == True:
+spelare1_vinster = 0
+spelare2_vinster = 0
+for i in range(3):
     spelare1_slag = randint(1, 6)
     spelare2_slag = randint(1, 6)
     if spelare1_slag > spelare2_slag:
         print(f"{spelare1} krossar den usla datormaskinen!")
+        spelare1_vinster += 1
     elif spelare2_slag > spelare1_slag:
         print("Spelare 2 är dig övermäktig, muhahah!")
+        spelare2_vinster += 1
     else:
         print("Meh, lika")
-    spela_igen = input("Vill du spela en runda till? [J/n] ")
-    if spela_igen == "n":
-        spela = False
-# skapa en variabel eller två variabler för att komma ihåg vem som har vunnit
+
+if spelare1_vinster > spelare2_vinster:
+    print("Grattis du vann besegrade datorn")
+elif spelare2_vinster > spelare1_vinster:
+    print("Datorn besegrade dig")
+else:
+    print("Attans, oavgjort")
